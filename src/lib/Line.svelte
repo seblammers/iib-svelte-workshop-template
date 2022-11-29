8c6d3c25054f771456363698b4e5ab172a674c50
Sebastian Lammers <36294705+seblammers@users.noreply.github.com>
2022-11-29 19:35:02 +0000

    new commit

---
+++ src/lib/Line.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let xScale;
+    export let yScale;
+    export let data;
+    export let color;
+
+    import { line } from "d3-shape";
+    const lineGenerator = line()
+        .x((d) => xScale(new Date(d.date)))
+        .y((d) => yScale(d.winprob));
+</script>
+
+<path
+    d={lineGenerator(data)}
+    fill="transparent"
+    stroke={color}
+    stroke-width="4"
+/>
